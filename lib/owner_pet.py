cd0c64cc22ac7e06950f79c99e274240e5eac1e3
joyce wamocho <joywamo@gmail.com>
2024-12-06 19:09:49 +0300

    one to many relationship

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,39 @@
 class Pet:
-    pass
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all =[]
+    def __init__(self,name,pet_type,owner=None):
+        self.name=name
+        self.pet_type=pet_type
+        self.owner =owner
+        if owner:
+            owner.add_pet(self)
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    @pet_type.setter
+    def pet_type(self,pet_type):
+        if pet_type not in Pet.PET_TYPES:
+            raise ValueError("invalid pet type")
+        else:
+            self._pet_type=pet_type
+
+
 
 class Owner:
-    pass
+    def __init__(self,name):
+        self.name=name
+        self._pets=[]
+    def pets(self):
+        return self._pets
+    def add_pet(self, pet):
+        if not isinstance(pet,Pet):
+            raise Exception("pet must be an instance of pet class")
+            
+        pet.owner = self
+        self._pets.append(pet)         
+    def get_sorted_pets(self):
+         return sorted(self._pets, key=lambda pet: pet.name)
+
+
